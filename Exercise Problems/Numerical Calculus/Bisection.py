5c05b720e70c5586842aa9fe9576a944a2815c33
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-04-17 21:21:37 -0400

    standardized boilerplate code

--- Exercise Problems/Numerical Calculus/Bisection.py
+++ Exercise Problems/Numerical Calculus/Bisection.py
@@ -1,1 +1,7 @@
-print("Hello, World")
+iterations = 0
+root = "placeholder"
+# If the method successfuly finds the root
+print("The procedure was succesful.")
+print("The value of the root is :", root)
+# If the method fails to find the root
+print("The method failed after", iterations, "iterations")
--- Exercise Problems/Numerical Calculus/Newton.py
+++ Exercise Problems/Numerical Calculus/Newton.py
@@ -1,4 +1,7 @@
 iterations = 0
-
+root = "placeholder"
+# If the method successfuly finds the root
+print("The procedure was succesful.")
+print("The value of the root is :", root)
 # If the method fails to find the root
 print("The method failed after", iterations, "iterations")
