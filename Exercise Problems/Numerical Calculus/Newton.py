231c56c39597d862d48adcafbfb09e3bf81d0051
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-04-22 23:31:02 -0400

    Got Newton's method working I think?

--- Exercise Problems/Numerical Calculus/Newton.py
+++ Exercise Problems/Numerical Calculus/Newton.py
@@ -1,15 +1,48 @@
-global x0 = 
-iterations = 0
-tolerance = 0.01
-root = "placeholder"
+# Import the library necessary for math functions
+import math
 
-difference =
-# Loop that calculates the root using the Bisection Method
-while ( difference >= tolerance):
-    iterations += 1
-    y = x
-# If the method successfuly finds the root
-print("The procedure was succesful.")
-print("The value of the root is :", root)
+# Initial approximation
+initial = -200
+
+# This represents our polynomial: x^2 - x^2 + 2
+f = lambda x: x**3 - x**2 + 2
+
+# First derivative of f(x):
+first_deriv = lambda x: 3*x**2 - 2*x
+
+
+# Set our root initially to a null value as a placeholder
+root = None
+
+# Set our number of iterations as 0
+iterations = 0
+max = 15
+# The tolerance for the function
+tolerance = 0.01
+
+# Loop that calculates the root using Netwon's Method as long as the
+# iterations have not exceeded their max
+while (iterations < max):
+    # Check if the derivative is 0, as Newton's method stipulates
+    # that f'(P_n-1) cannot be 0 at any point
+    if (first_deriv(initial) != 0):
+        # We set the next to be equal to the previous minus f(prev) divided by f'(prev)
+        next = initial - f(initial) / first_deriv(initial)
+
+        # If the method successfuly finds the root
+        if (abs(next - initial) < tolerance):
+            # The next approximation will be the root we want
+            root = next
+            print("The procedure was succesful.")
+            print("The value of the root is :", root)
+            exit()
+
+
+        iterations += 1
+        initial = next
+
+
+
 # If the method fails to find the root
-print("The method failed after", iterations, "iterations")
+if (root == None):
+    print("The method failed after", iterations, "iterations")
