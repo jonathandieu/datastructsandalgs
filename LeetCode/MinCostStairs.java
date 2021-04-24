dc3968590e185c62267347f9485fb451254ab5f5
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-04-24 04:05:03 -0400

    Added an easy dynamic programming problem

---
+++ LeetCode/MinCostStairs.java
@@ -0,0 +1,18 @@
+class MinCostStairs
+{
+    public static int minCostClimbingStairs(int[] cost)
+    {
+        // Initial Cost
+        float total = 0;
+        for (int i = 0; i < cost.length; i++)
+        {
+            total = cost[i] + 
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[] input = new int[]{ 10, 15, 20};
+        minCostClimbingStairs(input);
+    }
+}
