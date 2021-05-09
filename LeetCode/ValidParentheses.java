3c1fdcfc7efaebbea66b9897b36f099d3acd315d
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-05-09 02:51:27 -0400

    Set up the problem statement

---
+++ LeetCode/ValidParentheses.java
@@ -0,0 +1,14 @@
+public class ValidParentheses
+{
+    // Given a string s containing just the characters '(', ')', '{', '}', '[', and ']'
+    // determine if the input string is valid
+
+    public boolean isValid(String s)
+    {
+        // String is valid if:
+        // 1. Open brackets must be closed by the same type of brackets
+        // 2. Open brackets must be closed in the right order
+
+
+    }
+}
