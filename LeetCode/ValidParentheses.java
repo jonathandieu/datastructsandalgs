674a1c6482e446b5f97100cd2bd113038c79327c
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-05-12 23:59:31 -0400

    Started implementation with turning string into char array

--- LeetCode/ValidParentheses.java
+++ LeetCode/ValidParentheses.java
@@ -1,14 +1,26 @@
+import java.util.Arrays;
+
 public class ValidParentheses
 {
     // Given a string s containing just the characters '(', ')', '{', '}', '[', and ']'
     // determine if the input string is valid
 
-    public boolean isValid(String s)
+    public static boolean isValid(String s)
     {
         // String is valid if:
         // 1. Open brackets must be closed by the same type of brackets
         // 2. Open brackets must be closed in the right order
 
+        char[] charray = s.toCharArray();
+        System.out.println("Ok here we go: " + Arrays.toString(charray));
 
+        return false;
+
+    }
+
+    public static void main(String[] args)
+    {
+        String s = "bruh";
+        isValid(s);
     }
 }
