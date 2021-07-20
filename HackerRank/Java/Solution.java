540b1022372b1ebe1ed2cadd74b63fd7a96af768
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-07-19 21:21:27 -0400

    starting java section on hackerrank

---
+++ HackerRank/Java/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
