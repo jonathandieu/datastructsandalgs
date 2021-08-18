590845927549f45d7c74b841e8ebf31c5e24e7e6
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-08-18 00:43:14 -0400

    Set up first test case skeleton

---
+++ Codepath/Assessment.java
@@ -0,0 +1,12 @@
+class Assessment
+{
+    public static void main(String[] args)
+    {
+        System.out.println(test1() ? "Test 1: Passed" : "Test 1: Failed");
+    }
+
+    public static boolean test1()
+    {
+        return false;
+    }
+}
