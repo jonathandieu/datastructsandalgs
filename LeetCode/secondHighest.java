ab2e267e69281a2ae7450ac68a77a4e9e7d6a1b5
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-08-17 21:01:59 -0400

    Setting up the problem statement'

---
+++ LeetCode/secondHighest.java
@@ -0,0 +1,9 @@
+class secondHighest
+{
+    // Given an alphanumeric string s, return the second largest numerical digit that appears in s, or -1 if it doesn't exist.
+    public int secondHighest(String s)
+    {
+        return -1;
+    }
+
+}
