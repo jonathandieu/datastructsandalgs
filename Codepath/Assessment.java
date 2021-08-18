70c2462ec32c642f542175841efd76e830ba607f
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-08-18 18:37:42 -0400

    increased number of test cases

--- Codepath/Assessment.java
+++ Codepath/Assessment.java
@@ -3,9 +3,21 @@ class Assessment
     public static void main(String[] args)
     {
         System.out.println(test1() ? "Test 1: Passed" : "Test 1: Failed");
+        System.out.println(test2() ? "Test 1: Passed" : "Test 1: Failed");
+        System.out.println(test3() ? "Test 1: Passed" : "Test 1: Failed");
     }
 
     public static boolean test1()
+    {
+        return false;
+    }
+
+    public static boolean test2()
+    {
+        return false;
+    }
+
+    public static boolean test3()
     {
         return false;
     }
