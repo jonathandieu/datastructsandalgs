5b894629403e0adaecb9dfe4d0a07c43992dd280
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-08-19 22:25:22 -0400

    Fixed test-case output

--- Codepath/Assessment.java
+++ Codepath/Assessment.java
@@ -3,8 +3,8 @@ class Assessment
     public static void main(String[] args)
     {
         System.out.println(test1() ? "Test 1: Passed" : "Test 1: Failed");
-        System.out.println(test2() ? "Test 1: Passed" : "Test 1: Failed");
-        System.out.println(test3() ? "Test 1: Passed" : "Test 1: Failed");
+        System.out.println(test2() ? "Test 2: Passed" : "Test 2: Failed");
+        System.out.println(test3() ? "Test 3: Passed" : "Test 3: Failed");
     }
 
     public static boolean test1()
