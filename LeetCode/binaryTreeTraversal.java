efb5295e46d14c793791cf3c4b36bfbd757f2281
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-08-31 09:08:14 -0400

    fixed naming error

--- LeetCode/binaryTreeTraversal.java
+++ LeetCode/binaryTreeTraversal.java
@@ -1,28 +1,27 @@
-class binaryTreeTraversal.java
+class binaryTreeTraversal
 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // EXAMPLE:    1
+            //              \
+            //               2
+            //              /
+            //             3
+            //            / \
+            //          null null
 
- public List<Integer> inorderTraversal(TreeNode root) {
-      // EXAMPLE:    1
-        //              \
-        //               2
-        //              /
-        //             3
-        //            / \
-        //          null null
-
-        // Function call 1: [1, ]
-        // First recursive call: [2,]
-        // Second recursive call: [3, ]
-        List traversal = new ArrayList<Integer>();
-        // Base Case where we've reached the bottom of the tree
-        if (root == null)
-            return traversal;
-        else
-        {
-            traversal.addAll((inorderTraversal(root.left))); // Left
-            traversal.add(root.val); // Middle
-            traversal.addAll((inorderTraversal(root.right))); // Right
+            // Function call 1: [1, ]
+            // First recursive call: [2,]
+            // Second recursive call: [3, ]
+            List traversal = new ArrayList<Integer>();
+            // Base Case where we've reached the bottom of the tree
+            if (root == null)
+                return traversal;
+            else
+            {
+                traversal.addAll((inorderTraversal(root.left))); // Left
+                traversal.add(root.val); // Middle
+                traversal.addAll((inorderTraversal(root.right))); // Right
+            }
+            return traversal; // Complete list
         }
-        return traversal; // Complete list
-    }
 }
