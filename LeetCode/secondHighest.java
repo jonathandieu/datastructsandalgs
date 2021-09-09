5d60db389ea8209fd010b4b3a9a61ba718ee6033
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-09-08 23:59:01 -0400

    almost working solution, finishing logic

--- LeetCode/secondHighest.java
+++ LeetCode/secondHighest.java
@@ -3,8 +3,17 @@ class secondHighest
     // Given an alphanumeric string s, return the second largest numerical digit that appears in s, or -1 if it doesn't exist.
     public int secondHighest(String s)
     {
-        int[] nums = new int[10];
-        return -1;
+        // int[] nums = new int[10];
+        // return -1;
+
+        charArray = s.toCharArray();
+        for (int i = 0; i < charArray.length; i++)
+        {
+            if (Character.isDigit(charArray[i]))
+            {
+
+            }
+        }
     }
 
 }
