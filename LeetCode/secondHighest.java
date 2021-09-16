a63b8fde18324a4e86ea9703b5cd981768134894
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-09-16 13:33:37 -0400

    implemented reverse and swap cases in python

--- LeetCode/secondHighest.java
+++ LeetCode/secondHighest.java
@@ -3,17 +3,7 @@ class secondHighest
     // Given an alphanumeric string s, return the second largest numerical digit that appears in s, or -1 if it doesn't exist.
     public int secondHighest(String s)
     {
-        // int[] nums = new int[10];
-        // return -1;
-
-        charArray = s.toCharArray();
-        for (int i = 0; i < charArray.length; i++)
-        {
-            if (Character.isDigit(charArray[i]))
-            {
-
-            }
-        }
+        return -1;
     }
 
 }
