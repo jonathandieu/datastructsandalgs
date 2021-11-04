4aafd95e531ef147177f5f95c316ed7cbcafd7fc
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-11-04 19:22:40 -0400

    successful submission implemented

--- LeetCode/minimum_valid_parentheses.py
+++ LeetCode/minimum_valid_parentheses.py
@@ -1,2 +1,18 @@
 def minimum_valid_parentheses(s: string) -> string:
-    pass
+   stack = []
+        s = list(s)
+        # Iterate through the string and check for parentheses
+        for index, char in enumerate(s):
+            # If the character is an open parenthesis:
+            if char == '(':
+                stack.append(index) # append the index to the stack
+            # If the char is a closing parenthesis:
+            if char == ')':
+                if len(stack) != 0: # when the stack has elements, we pop from the stack and it remains valid
+                    stack.pop()
+                    continue # make sure we continue after popping because we don't want to check if the stack is empty on the same iteration
+                if stack == []:
+                    s[index] = ""
+        for index in stack:
+            s[index] = ''
+        return "".join(s)
