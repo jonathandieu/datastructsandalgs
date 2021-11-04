9d1cfbf9ff6724ac6285cf32f75af3e25b15f2cc
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-11-03 23:59:09 -0400

    facebook prep question

---
+++ LeetCode/max_area_of_island.py
@@ -0,0 +1,11 @@
+# Dimensions of the grid
+def print_enum_list(listy: list) -> None:
+    for i, element in enumerate(listy):
+        print(f"i: {i}, element: {element}", end="yert")
+
+def max_area_of_island(grid: list[list[int]]):
+    # This is my nested comment
+    pass
+
+my_list = ["Jon", "Nina", "Bean", "David", "Sho", "Justin", "Ariel"]
+print_enum_list(my_list)
