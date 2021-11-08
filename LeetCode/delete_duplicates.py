11a699c80aca8c2da12e80e5bec4a3db85de325d
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-11-07 23:56:05 -0500

    added delete_duplicates.py and implemented class definition of a node

---
+++ LeetCode/delete_duplicates.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, data: int=0, next=None):
+        self.data = data
+        self.next = next
+
+
+def delete_duplicates(head: Optional[ListNode]):
+    pass
