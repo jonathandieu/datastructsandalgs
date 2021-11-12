f999dd090287b50add1a12cd224434bf317de2d2
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-11-12 01:08:11 -0500

    Started function signature and class definition

---
+++ LeetCode/tree_to_doubly.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+def tree_to_doubly(root: Node) -> Node:
+    pass
+
+node = Node(3)
+print(node)
+tree_to_doubly(node)
