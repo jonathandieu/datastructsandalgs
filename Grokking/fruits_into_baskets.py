59d6a86169b7cf8223ec202ab404b24df5ebfd88
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-12-19 23:10:06 -0500

    Added problem description and function signature

---
+++ Grokking/fruits_into_baskets.py
@@ -0,0 +1,12 @@
+# You can only have TWO baskets that hold a single type of fruit
+# No limit on the amount of fruit
+# Pick exactly one fruit from every tree (including the start tree) moving to the right
+# Stop when we reach a tree with fruit you cannot fit in your baskets
+# Return the MAX number of fruits you can pick
+
+# ? Sliding window: use when traversing an array/string/data structure and having some subsequence of the structure as the answer
+# Most of the time just increment the window until you reach a breach of some condition and then decrement the window until the condition is true again
+
+# Will sorting this array help us? It might make it so that
+def total_fruit(fruits: list[int]) -> int:
+    pass
