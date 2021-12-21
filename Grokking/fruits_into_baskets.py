9e03d50f2d193794ad5877d4c030d72b18b61dd6
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-12-20 23:25:34 -0500

    Altered problem description

--- Grokking/fruits_into_baskets.py
+++ Grokking/fruits_into_baskets.py
@@ -8,5 +8,5 @@
 # Most of the time just increment the window until you reach a breach of some condition and then decrement the window until the condition is true again
 
 # Will sorting this array help us? It might make it so that
+# On second thought, sorting the array will destroy the order of the array, which we can't do because we're obligated to take the fruit from a tree no matter what.
 def total_fruit(fruits: list[int]) -> int:
-    pass
