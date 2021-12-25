42d47397e3f40de63eab262408a5e9a3fe89e855
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-12-24 23:23:44 -0500

    fixed typing issue

--- LeetCode/Blind 75/Arrays/maximum_subarray.py
+++ LeetCode/Blind 75/Arrays/maximum_subarray.py
@@ -1,2 +1,3 @@
 # Given an integer array nums, find the contiguous subarray (containing >= 1 number) which has the largest sum and return its sum
-def max_subarray(nums: list[int]) -> int:
+def max_subarray(nums: List[int]) -> int:
+    pass
