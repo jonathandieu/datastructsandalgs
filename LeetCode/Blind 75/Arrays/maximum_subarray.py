a64b631eaa14533b1e0e3b53e2f3754c100d79ba
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-01-01 18:23:34 -0500

    incremental changes

--- LeetCode/Blind 75/Arrays/maximum_subarray.py
+++ LeetCode/Blind 75/Arrays/maximum_subarray.py
@@ -1,4 +1,5 @@
 # Given an integer array nums, find the contiguous subarray (containing >= 1 number) which has the largest sum and return its sum
 def max_subarray(nums: List[int]) -> int:
+    # We want to return a subarray, so we initialize that here
     return_array = []
-    pass
+    return return_array
