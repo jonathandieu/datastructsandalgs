3841a8907290fb0286e12996a6654f4add363f08
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-01-07 23:24:53 -0500

    updated instructions to reflect new chalenge

--- LeetCode/find_kth_largest.py
+++ LeetCode/find_kth_largest.py
@@ -2,3 +2,4 @@ def find_kth_largest(nums: List[int], k: int) -> int:
     nums.sort()
     reverse = nums[::-1]
     return reverse[k - 1] # Due to indexing we return the kth place - 1
+    # This is kind of a cop-out: try to do it some other way
