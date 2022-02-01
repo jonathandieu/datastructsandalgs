b1fa6e47f260838d6929c5f457d5b1c400960a51
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-01-31 23:30:10 -0500

    Started working on Walmart's code challenge to implement a version of a max heap

---
+++ Data Structures/Walmart-Forage/powerOfTwoMaxHeap.java
@@ -0,0 +1,19 @@
+public class powerOfTwoMaxHeap
+{
+    int max;
+
+    // Constructor
+    private powerOfTwoMaxHeap(int number)
+    {
+        this.max = number;
+    }
+
+    private class heapNode
+    {
+
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("We in BUSINESS");
+    }
+}
