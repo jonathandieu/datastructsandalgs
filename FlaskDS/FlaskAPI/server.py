07cfc32712a3068d5c7e3ecc42f1a522ad787472
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-02-06 23:52:31 -0500

    Setting up database wip

--- FlaskDS/FlaskAPI/server.py
+++ FlaskDS/FlaskAPI/server.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, jsonify
+from sqlite3 import Connection as SQLite3Connection
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+# from flask_sqlalchemy import SQLAlchemy
 
 # Application
 app = Flask(__name__)
@@ -6,3 +10,12 @@ app = Flask(__name__)
 # Configuration Dictionary
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
 app.config["SQL_TRACK_MODIFICATIONS"] = 0
+
+# Models
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    address = db.Column(db.String(50))
+    addressphone = db.Column(db.String(50))
