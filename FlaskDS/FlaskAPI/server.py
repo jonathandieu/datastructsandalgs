2d0eb66db21926c6eacbcc93dc8587d2a6474196
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-02-11 23:59:21 -0500

    More app config

--- FlaskDS/FlaskAPI/server.py
+++ FlaskDS/FlaskAPI/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from sqlite3 import Connection as SQLite3Connection
+from datetime import datetime
 from sqlalchemy import event
 from sqlalchemy.engine import Engine
 # from flask_sqlalchemy import SQLAlchemy
@@ -11,11 +12,20 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
 app.config["SQL_TRACK_MODIFICATIONS"] = 0
 
+# Configure sqlite3 to enforce foreign key constraints
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, _connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+        
 # Models
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(50))
-    address = db.Column(db.String(50))
+    address = db.Column(db.String(200))
     addressphone = db.Column(db.String(50))
+    addressphone = db.relationship("BlogPost")
