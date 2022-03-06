323ff7180529318eba25139c62df3e12d21a98fe
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-03-05 23:57:56 -0500

    Renamed exercise problems -> exercises and added csv practice

--- Exercises/csv_practice.py
+++ Exercises/csv_practice.py
@@ -6,10 +6,15 @@ if __name__ == "__main__":
     with open(file_path) as my_file: # Using "with" statement ensures that file will be closed afterwards.
 
         # Using list comprehension
-        data = [line for line in my_file]
-        print(f"This is using the List Comprehension Way: {data}")
+        # data = [line for line in my_file]
+        # print(f"This is using the List Comprehension Way: {data}")
 
-        csv_reader = csv.reader(my_file)
-        print(f"This is using the csv_reader way from the csv import: ")
-        for row in csv_reader:
-            print(row)
+        # csv_reader = csv.reader(my_file)
+        # print(f"This uses the csv_reader way from the csv import: ")
+        # for row in csv_reader:
+        #     print(row)
+
+        print("This uses the dictreader from the csv import: ")
+        dict_reader = csv.DictReader(my_file)
+        for line in dict_reader:
+            print(line)
