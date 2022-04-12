9986c8cd960f5c4963ee6bb4725c6b64d4fa584d
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-04-11 23:58:27 -0400

    reconciled merge conflicts

--- Grokking/Sliding Window/max_sub_array_of_size_k.py
+++ Grokking/Sliding Window/max_sub_array_of_size_k.py
@@ -17,11 +17,36 @@ def optimal_max_sub_array_of_size_k(k: int, arr: list) -> list:
             window_start += 1
     print(max_sum)
 
+def max_sub_array_of_size_k(k, arr):
+  # TODO: Write your code here
+  # Initialize maxes to 0
+  window_sum = 0 # 6
+  max_sum = 0 # 7
+  window_start = 0 # 3
+
+  # Loop through the length of the array with the window
+  for window_end in range(len(arr)): # THISSSS IS WHERE N RUNTIME COMES FROM FOR LOOP ITERATIONS: 0 1 2 3 4
+    window_sum += arr[window_end]
+
+    # Check if the window has reached k size
+    if (window_end + 1) >= k: # Plus 1 for indexing
+      max_sum = max(window_sum, max_sum)
+      window_sum -= arr[window_start]
+      window_start += 1
+
+  return max_sum
+
 # driver function
 if __name__ == "__main__":
     test_list = [2, 3, 4, 1, 5]
-    optimal_max_sub_array_of_size_k(2, test_list)
+    # optimal_max_sub_array_of_size_k(2, test_list)
+    print(max_sub_array_of_size_k(2, test_list))
 
+
+
+# Runtime Analysis
+# Go through the length of array: O(n)
+# Space: No auxilliary data structures: O(1)
 
 # list: [1, 2, 3, 4, 5]
 # k = 1 answer = 5
--- Grokking/Sliding Window/smallest_subarray_with_a_greater_sum.py
+++ Grokking/Sliding Window/smallest_subarray_with_a_greater_sum.py
@@ -2,19 +2,39 @@ def smallest_subarray_with_a_greater_sum(arr: list, S: int):
     """
     Given an array of positive numbers and a positive number ‘S,’ find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
     """
-    # Initialize global min and window_sum
-    min_size = 0
-    window_sum = 0
-    window_start = 0
-#                 0  1  2  3  4  5
-    #test_list = [7, 1, 5, 2, 3, 2]
+    # Initialize global min to be a number we never reach and window_sum
+    min_size = len(arr) + 1 # answer we are returning
+    window_size = 0 # compare to the min_size each time
+    window_sum = 0 # gets compared to S      7!
+    window_start = 0 # Butt of the catepillar
+
+    #               0  1  2  3  4  5 Indices of the array
+    # test_list1 = [7, 1, 5, 2, 3, 2] S = 7 EXPECTED = 1
+    # test_list2 = [2, 1, 5, 2, 3, 2] EXPECTED = 2
+    #
 
     # For loop to move the window along, end being the index in range()
-    for window_end in range(len(arr)): # 0,1,2,3,4,5
-        window_sum += arr[window_end] # arr[0]
+
         # if >= S:
 
+    for window_end in range(len(arr)): # 0, 1, 2, 3, 4, 5
+        window_sum += arr[window_end] # arr[0] = 7
+        window_size += 1
+        if window_sum >= S:
+            min_size = min(window_size, min_size)
+            window_sum -= arr[window_start]
+            window_start += 1
+            window_size -= 1
+            if window_sum >= 7:
+                min_size = min(min_size, window_size)
+
+
+
     return min_size
+
+
+    return 0
+
 
 def smallest_subarray_sum(arr, s):
     min_size = len(arr) + 1 # answer we are returning (initialized to something we'll never reach)
@@ -34,6 +54,7 @@ def smallest_subarray_sum(arr, s):
 
 if __name__=="__main__":
     test_list2 = [7, 1, 5, 2, 3, 2]
-    test_list1 = [2, 1, 5, 2, 3, 2]
-    # print(smallest_subarray_with_a_greater_sum(test_list1, 7))
-    print(smallest_subarray_sum(test_list1, 7))
+
+    test_list1 = [2, 1, 5, 2, 3, 8]
+    # print(smallest_subarray_with_a_greater_sum(test_list2, 7), "Expected: 1")
+    print("output:", smallest_subarray_with_a_greater_sum(test_list1, 7), "Expected: 1")
