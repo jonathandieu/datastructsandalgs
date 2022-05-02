f8056e67cbfe82e156c622732ea2d8ff83e13c11
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-05-01 23:59:19 -0400

    working solution

--- LeetCode/Sliding Window/longest_substring_without_repeating_chars.py
+++ LeetCode/Sliding Window/longest_substring_without_repeating_chars.py
@@ -1,15 +1,38 @@
-def lengthOfLongestSubstring(self, s: str) -> int:
-    frequency = {} # Dictionaries are key:value i.e character:count
-
-    # WITHOUT REPEATING CHARS == FreqCount <= 1
-    window_start, max_length = 0, 0 # Initialize start and max_length to compare
+from enum import unique
 
 
-    for window_end in s:
-        # If a character has been repeated, we move the window_start - else: we move the window_end
-        if frequency[window_end]:
-            return
-        # Character has been repeated if its count
-        current_length = window_end - window_start
+def length_of_longest_substring(s: str) -> int:
+    # Use the sliding window pattern:
+
+    # Use a set to keep track of unique chars
+    unique_chars = set()
+    # Initialize values
+    max_length = 0
+    window_start = 0
+
+    # for window_end in range(len(s)):
+    #     if s[window_end] not in unique_chars:
+    #         unique_chars.add(s[window_end])
+    #         max_length = max(max_length, (window_end - window_start) + 1)
+
+    #     else:
+    #         unique_chars.remove(s[window_start])
+    #         window_start += 1
+    # return max_length
+
+    for window_end in range(len(s)):
+        while s[window_end] in unique_chars:
+            unique_chars.remove(s[window_start])
+            window_start += 1
+
+        unique_chars.add(s[window_end])
+        max_length = max(max_length, window_end - window_start + 1)
 
     return max_length
+
+
+
+
+if __name__ == "__main__":
+    s = "bbbbb"
+    print(length_of_longest_substring("bbbbb"))
