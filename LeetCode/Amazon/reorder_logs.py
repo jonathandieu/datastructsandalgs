c9557ed3774c6216516e40d357fc7514c7b71e10
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-05-04 23:57:49 -0400

    Added pseudocode and function signature

---
+++ LeetCode/Amazon/reorder_logs.py
@@ -0,0 +1,14 @@
+
+def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        # Return final order of the logs such that:
+        # Letter logs come before digit logs
+        # Letter logs are sorted by their contents: if they're the same, then sort by identifier
+        # Digit logs must maintain their relative ordering
+
+
+        # Separate logs into letters and digits
+
+        # Sort by letters before digits
+        # Sort by contents of the letter logs
+        # Sort by identifier if contents are the same
+        pass
