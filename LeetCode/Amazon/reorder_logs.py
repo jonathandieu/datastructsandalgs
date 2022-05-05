b55f4fb0fb70eca270deb32828521897f0cf707c
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-05-05 00:27:20 -0400

    Complete solution with supalegible code

--- LeetCode/Amazon/reorder_logs.py
+++ LeetCode/Amazon/reorder_logs.py
@@ -7,8 +7,19 @@ def reorderLogFiles(self, logs: List[str]) -> List[str]:
 
 
         # Separate logs into letters and digits
+        letter_logs = []
+        number_logs = []
+
+        for log in logs:
+            if log.split(" ")[1].isdigit():
+                number_logs.append(log)
+            else:
+                letter_logs.append(log)
 
         # Sort by letters before digits
         # Sort by contents of the letter logs
         # Sort by identifier if contents are the same
-        pass
+        letter_logs.sort(key= lambda (log.split()[1:], log.split()[0])) # Sort with multiple criteria, descending priority: first is contents, second is identifier
+
+        sorted_logs = letter_logs + number_logs
+        return sorted_logs
