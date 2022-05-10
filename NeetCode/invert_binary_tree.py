6744ab0db6b3ba1e4f6e8f926f5a07bb91fbccd3
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-05-09 23:15:13 -0400

    Added problem statement and skeleton signature

---
+++ NeetCode/invert_binary_tree.py
@@ -0,0 +1,15 @@
+'''
+Given the root of a binary tree, invert the tree, and return its root.
+'''
+
+def invert_binary_tree(root):
+
+    # Base case:
+    if not root:
+        return None
+    # Invert the left subtree
+
+    # Invert the right subtree
+
+
+    pass
