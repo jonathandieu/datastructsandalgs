feb12e97e0a6f3507155e1ce321fa8db15d082eb
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-05-13 23:51:25 -0400

    solution with no explanation

--- NeetCode/is_balanced.py
+++ NeetCode/is_balanced.py
@@ -1,3 +1,15 @@
 # Return whether or not the binary search tree is balanced.
 def is_balanced(root):
-	pass
+	# Helper function that returns -1 if not balanced
+	def check(root):
+		if not root:
+			return 0
+		else:
+			left = check(root.left)
+			right = check(root.right)
+			if left == -1 or right == -1 or abs(left - right) > 1:
+				return -1
+
+			return 1 + max(left, right)
+
+		return check(root) != 1
