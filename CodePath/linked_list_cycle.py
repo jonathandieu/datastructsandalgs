759c2fb5d6529d15941f0e9d06736e7ab0200ca3
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-05-24 19:40:05 -0700

    Added function signature and problem description

---
+++ CodePath/linked_list_cycle.py
@@ -0,0 +1,12 @@
+'''
+Given the head of a linked list, determine whether
+the linked list has a cycle in it.
+
+A cycle is present if there is some node in the list that can
+be reached again by continuously following the next pointer.
+
+Return true if there is a cycle, and false otherwise.
+'''
+
+def has_cycle(head):
+    pass
