975a6e9ed484d38fba46e934065eab75cfb2c47b
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-05-29 22:04:32 -0700

    Added function signature and description

---
+++ CodePath/Linked Lists/delete_node.py
@@ -0,0 +1,7 @@
+'''
+Write a function to delete a node in a singly-linked list
+given access to the node to be deleted directly, not the HEAD.
+It's guaranteed that the node to be deleted is NOT the tail node.
+'''
+def delete_node(node):
+	pass
