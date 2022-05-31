d8e44ebee7882cf92766bed69aff0e7587483abf
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-05-30 21:32:20 -0700

    Finished solution working two-liner

--- CodePath/Linked Lists/delete_node.py
+++ CodePath/Linked Lists/delete_node.py
@@ -3,5 +3,9 @@ Write a function to delete a node in a singly-linked list
 given access to the node to be deleted directly, not the HEAD.
 It's guaranteed that the node to be deleted is NOT the tail node.
 '''
+
+# Because we aren't given the HEAD of the list, we must instead alter
+# the actual values of the linked list and remove the node with next references.
 def delete_node(node):
-	pass
+	node.value = node.next.value
+	node.next = node.next.next
