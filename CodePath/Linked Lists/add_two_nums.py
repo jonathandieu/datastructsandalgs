2a66d720ffa8cf8b386ad04e9a5f9fd7032a0ab6
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-06-02 20:57:47 -0700

    Created function signature + added description for the problem

---
+++ CodePath/Linked Lists/add_two_nums.py
@@ -0,0 +1,8 @@
+'''
+Given two non-empty linked lists that represent two non-negative
+integers. The digits are stored in reverse order, and each of their
+nodes contains one digit. Add the two numbers and return their sum as a linked list.
+'''
+# If we want to return the sum as a linked list,
+def add_two_nums(head) -> int:
+	pass
