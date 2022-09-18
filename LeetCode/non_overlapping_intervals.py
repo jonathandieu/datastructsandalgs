5b5db8fe4c4c0f0b58fbb02bcebb683a11a9f65c
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-09-18 00:00:05 -0400

    Complete solution

--- LeetCode/non_overlapping_intervals.py
+++ LeetCode/non_overlapping_intervals.py
@@ -1,2 +1,9 @@
 def non_overlapping_intervals(intervals) -> int:
-	return 0
+	end = float('-inf')
+	removed_intervals = 0
+	for interval_start, interval_end in sorted(intervals, key=lambda x: x[1]):
+		if interval_start >= interval_end:
+			end = interval_end
+		else:
+			removed_intervals += 1
+	return removed_intervals += 1
