5d0a2b9c32a2e1fc3430d11152fbb6b2e22eb19f
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-02-07 22:30:15 -0900

    Attempt to help gf with her CS1 assignment

---
+++ Exercises/girlfren.py
@@ -0,0 +1,27 @@
+# How do we track the valid ship combinations?
+# Idea: Have an array? 2D?
+
+# Check for midpoint
+
+# Move ships after each photo
+
+'''
+Your program should guess the “midpoint”. The midpoint is a bit unusual in this
+problem. At each time the photo is taken there is a list of possible locations that the ship could
+be (based on the speed/location combinations that have not been invalidated).
+'''
+
+
+# First photo is exactly 1 hour after distress signal
+# The ship will move for 1 hour after the last picture
+# After a certain point, you're taking pictures after the boat has passed. You're trying to catch up to it.
+def solve(S: int, L: int):
+
+    numberPictures: int = 24
+
+
+    # At 0, no pics have been taken; boat will be at starting position.
+    # If our HI is maxStartPos + maxSpeed, then we cover the max range that the boat could be in during our first photo
+
+
+    return 0
