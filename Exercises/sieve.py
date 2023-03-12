90895fa1be736b3eb17f05bfa47a16d511837d7f
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-03-12 09:28:22 -0900

    Start implementing is_prime and list comprehension function signature

--- Exercises/sieve.py
+++ Exercises/sieve.py
@@ -1,2 +1,12 @@
 def sieve(num: int):
     pass
+
+def prime_comprehension(num: int):
+    return [i for i in range(num) if is_prime(i)]
+
+
+def is_prime(num: int):
+    for x in range(2, num):
+        if num % x == 0:
+            return False
+    return True
