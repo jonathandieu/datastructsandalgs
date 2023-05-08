a771fb55cd89dec5d32506c4335547f6e5553e50
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-05-07 22:15:03 -0900

    wip: uncomment and format

--- NeetCode/encode_decode_strings.py
+++ NeetCode/encode_decode_strings.py
@@ -1,15 +1,17 @@
 class Codec:
     def encode(self, strs: List[str]) -> str:
-        """Encodes a list of strings to a single string.
+        """
+        Encodes a list of strings to a single string.
         """
 
 
     def decode(self, s: str) -> List[str]:
-        """Decodes a single string to a list of strings.
+        """
+        Decodes a single string to a list of strings.
         """
 
 
 
 # Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(strs))
+codec = Codec()
+codec.decode(codec.encode(strs))
