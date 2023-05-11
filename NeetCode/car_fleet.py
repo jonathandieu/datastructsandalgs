d715a03e2fbd3055509b76daacd71430efaf9696
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-05-10 20:50:43 -0900

    wip: add function docs

--- NeetCode/car_fleet.py
+++ NeetCode/car_fleet.py
@@ -1,2 +1,5 @@
 def car_fleet(self, target: int, position: list[int], speed: list[int]) -> int:
+    """
+    Returns the number of car fleets that will arrive at the destination.
+    """
     pass
