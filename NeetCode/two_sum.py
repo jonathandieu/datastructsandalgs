a4b666dc8bd931d984684e07926e5e82253ba806
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-05-26 19:59:51 -0900

    wip: start hashmap approach

--- NeetCode/two_sum.py
+++ NeetCode/two_sum.py
@@ -2,4 +2,5 @@ def two_sum(self, nums: list[int], target: int) -> list[int]:
     """
     Given a target number and a list of numbers, return a list of indices of numbers that add up to the target number.
     """
+    hash_map = {}
     pass
