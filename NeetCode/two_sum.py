7f99f89a932458d7a882707831a3cfa1319d0fb4
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-05-27 20:20:20 -0900

    wip: desribe solution with hashmap approach

--- NeetCode/two_sum.py
+++ NeetCode/two_sum.py
@@ -1,6 +1,7 @@
 def two_sum(self, nums: list[int], target: int) -> list[int]:
     """
-    Given a target number and a list of numbers, return a list of indices of numbers that add up to the target number.
+    Problem: Given a target number and a list of numbers, return a list of indices of numbers that add up to the target number.
+    Solution: Iterate through the list of nums and subtract the target from each number and save it in the hash map for reference.
     """
     hash_map = {}
     pass
