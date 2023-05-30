da98ba261cbba725cba1619e54fe9c8b4f6b9659
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-05-29 19:40:42 -0900

    wip: add explanation for time complexity

--- NeetCode/two_sum.py
+++ NeetCode/two_sum.py
@@ -2,6 +2,7 @@ def two_sum(self, nums: list[int], target: int) -> list[int]:
     """
     Problem: Given a target number and a list of numbers, return a list of indices of numbers that add up to the target number.
     Solution: Iterate through the list of nums and subtract the target from each number and save it in the hash map for reference.
+    Time complexity: O(n), where n is the number of items in the list we must iterate through.
     """
     hash_map = {}
     pass
