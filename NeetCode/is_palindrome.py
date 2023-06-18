db0255a0851b33cb1b35cd7412e2319ca7a50069
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-06-17 23:44:40 -0700

    Add helper function signature

--- NeetCode/is_palindrome.py
+++ NeetCode/is_palindrome.py
@@ -3,3 +3,9 @@ def is_palindrome(s: str) -> bool:
     Given a string sequence, determines whether or not it's a palindrome
     """
     pass
+
+def parse_string(s: str) -> bool:
+    """
+    Given a string sequence, returns string without whitespaace and punctuation.
+    """
+    pass
