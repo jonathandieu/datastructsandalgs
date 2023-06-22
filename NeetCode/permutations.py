a6b8c31431173180cb93b3f6f7e2e38d97508676
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-06-21 23:26:28 -0700

    added type annotations and docstrings for permutations

--- NeetCode/permutations.py
+++ NeetCode/permutations.py
@@ -1,2 +1,5 @@
-def permutations(nums):
+def permutations(nums: list[int]) -> list[list[int]]:
+	"""
+	Given an array nums of distinct integers, returns all the possible permutations. Can return the answer in any order.
+	"""
 	pass
