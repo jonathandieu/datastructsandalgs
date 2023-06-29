83689c2da7018392734e8f1c23a37af626cdbe2c
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-06-28 23:23:50 -0700

    comment general outline for the problem

--- NeetCode/car_fleet.py
+++ NeetCode/car_fleet.py
@@ -2,4 +2,6 @@ def car_fleet(self, target: int, position: list[int], speed: list[int]) -> int:
     """
     Returns the number of car fleets that will arrive at the destination.
     """
-    pass
+    # 1. Sort the vehicles by (position, velocity) pair
+    # 2. Use a stack to keep track of each vehicle
+    # 3, The stack should contain the arrival times of the fleets
