a394f54ead51955f1a80ac034e8443812415b4fa
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-11-12 23:31:44 -0500

    feat: add comments for code explaining solution

--- NeetCode/merge_sorted_linked_lists.py
+++ NeetCode/merge_sorted_linked_lists.py
@@ -3,9 +3,11 @@ class ListNode():
         self.val = val
         self.next = next
 def merge_sorted_linked_lists(list1, list2):
+    # Create a dummy node to avoid edge cases
     dummy = ListNode()
     curr = dummy
 
+    # Iterate through both linked lists, comparing their values
     while list1 and list2:
         if list1.val < list2.val:
             curr.next = list1
@@ -16,9 +18,11 @@ def merge_sorted_linked_lists(list1, list2):
 
         curr = curr.next
 
+    # Handle the case where the lists are not the same size, so leftover list1 or list2 needs to be appended
     if list1:
         curr.next = list1
     elif list2:
         curr.next = list2
 
+    # Return the list that was appended to the dummy node
     return dummy.next
