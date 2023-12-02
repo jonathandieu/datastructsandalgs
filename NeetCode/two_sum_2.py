f18da1a7c1f3eeb28db7ccdcf7d686e4edb6d25e
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-12-01 23:17:50 -0800

    chore: set up function signature and problem statement

---
+++ NeetCode/two_sum_2.py
@@ -0,0 +1,4 @@
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+# Return the indices.
+def twoSum(numbers: list[int], target: int) -> list[int]:
+    pass
